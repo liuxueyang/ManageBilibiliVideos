effee9b35ed088ef1d963b1732aa796daf77bc13
liuxueyang <zypz457@sina.com>
2017-03-18 10:47:21 +0800

    Update README.md. Add perl source code.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # ManageBilibiliVideos
 Manage imported downloaded videos from Bilibili Android client
+
+## How to use
+
+1. check the `entry.json` file in each directory to get the position of the video title.
+This is an example how to get the title of bonobono:
+```perl
+my $index = $data->{'ep'}{'index'} . $data->{'ep'}{'index_title'};
+```
+Another example about how to get the title of TOUCH:
+```perl
+my $index = $data->{'title'};
+```
+2. Update the source code at line 43 according to the json file.
+3. run the program:
+```bash
+perl ./touch.pl PATH_TO_DIRECTORY
+```
+`PATH_TO_DIRECTORY` is an absolute path to the directory using `adb pull` to import to computer. eg:
+```bash
+adb pull /storage/emulated/0/Android/data/tv.danmaku.bili/download/s_5615
+```
+In this case, PATH_TO_DIRECTORY is the absolute path to `s_5615`. Those renamed videos is placed to the directory where the script runs.
---
+++ touch.pl
@@ -0,0 +1,108 @@
+#!/usr/bin/perl
+
+# Date  : 2017/03/16 19:14:35
+# Finish: 2017/03/16 22:04:07
+
+# NOTE: There MUST NOT be any non-ascii character in the path!!!!!!!!!!
+
+use strict;
+use warnings;
+use 5.014;
+use Cwd;
+use JSON qw();
+use open ':std', ':encoding(UTF-8)';
+use File::Copy;
+
+my $cur_dir = $ARGV[0] or die "Usage: perl ./touch.pl DIRECTORYPATH";
+$cur_dir .= '/' unless ($cur_dir =~ '/$');
+
+# my $cur_dir = '/home/repl/Videos/Bilibili/Fanju/TOUCH/s_2425';
+# my $cur_dir = '/home/repl/Videos/MV/5028728/';
+
+opendir(DIR, $cur_dir) or dir $!;
+
+while (my $file = readdir(DIR)) {
+    next if ($file =~ /^\./);
+    
+    # get index_title
+    my $json_file = $cur_dir . "/$file" . '/entry.json';
+
+    my $json_text = do {
+	open(my $json_fh, "<:encoding(UTF-8)", $json_file)
+	    or die("Can not open $json_file\"$!\n\"");
+	local $/;
+	<$json_fh>
+    };
+
+    my $json = JSON->new;
+    my $data = $json->decode($json_text);
+
+    # get title from json file
+    
+    # bonobono
+    my $index = $data->{'ep'}{'index'} . $data->{'ep'}{'index_title'};
+    
+    # touch
+    # my $index = $data->{'title'};
+
+    # directories like 58116
+    my $subdir = "$cur_dir/$file";
+    opendir(SUBDIR, $subdir) or dir $!;
+
+    while (my $subfile = readdir(SUBDIR)) {
+	next if ($subfile =~ /^\./);
+	if (-d "$subdir/$subfile") {
+	    if ($subfile =~ /mp4/) {
+		# only rename
+		opendir(DSTDIR, "$subdir/$subfile");
+		while (my $dstfile = readdir(DSTDIR)) {
+		    next if ($dstfile =~ /^\./);
+		    if ($dstfile =~ /mp4$/) {
+			$index =~ s/ /_/g;
+			rename("$subdir/$subfile/$dstfile", "$index.mp4") or die
+			    "failed to copy file" . $index;
+		    }
+		}
+		closedir(DSTDIR);
+	    }
+	    if ($subfile =~ /flv/) {
+		# concat flv files
+		opendir(DSTDIR, "$subdir/$subfile");
+		my @flv = ();
+		while (my $dstfile = readdir(DSTDIR)) {
+		    next if ($dstfile =~ /^\./);
+		    if ($dstfile =~ /flv$/) {
+			push @flv, $dstfile;
+		    }
+		}
+		# if there are more than 1 flv videos: merge and rename
+		if (@flv > 1) {		    
+		    @flv = sort { $a cmp $b } @flv;
+		    @flv = map { "file './$_'" } @flv;
+		    my $mylist = "$subdir/$subfile/mylist.txt";
+		    open (my $fh, ">", $mylist)
+			or dir $!;
+		    for (@flv) { print $fh "$_\n"; }
+		    close($fh);
+		    say "$subdir/$subfile";
+		    # output file is: output.flv. Chinese filename will crash. T_T
+		    system("cd $subdir/$subfile && ffmpeg -f concat -safe 0 -i mylist.txt -c copy output.flv");
+		    # rename the filename of the output.flv to Chinese filename.
+		    $index =~ s/ /_/g;
+		    rename("$subdir/$subfile/output.flv", "$index.flv") or die
+			"failed to copy file" . $index;
+		}
+		else {
+		    $index =~ s/ /_/g;
+		    rename("$subdir/$subfile/$flv[0]", "$index.flv") or die
+			"failed to copy file" . $index;
+		}
+		closedir(DSTDIR);
+	    }
+	}
+    }
+    close(SUBDIR);
+}
+
+closedir(DIR);
+exit 0;
